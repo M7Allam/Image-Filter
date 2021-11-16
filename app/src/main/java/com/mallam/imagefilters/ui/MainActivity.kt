3846db64639397aa9369c1a9b6c4516de182baed
Mahmoud Allam <mahmoudallam2050@gmail.com>
2021-11-16 05:30:05 +0200

    Commit v1.1 {  changeBackground() MainActivity.kt }

--- app/src/main/java/com/mallam/imagefilters/ui/MainActivity.kt
+++ app/src/main/java/com/mallam/imagefilters/ui/MainActivity.kt
@@ -4,11 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
+import android.view.animation.AnimationUtils
+import com.mallam.imagefilters.R
 import com.mallam.imagefilters.databinding.ActivityMainBinding
+import com.mallam.imagefilters.utilities.BackgroundImages
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     //Companion Object
     companion object{
+        private var BACKGROUND_IMAGE_POSITION = 1
+        private var BREAK_LOOP = false
         private const val REQUEST_CODE_PICK_IMAGE = 1
         const val KEY_IMAGE_URi = "imageUri"
     }
@@ -25,6 +32,13 @@ class MainActivity : AppCompatActivity() {
 
         //Buttons Click Listeners
         setClickListeners()
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        BREAK_LOOP = false
+        changeBackgroundImage()
     }
 
     private fun setClickListeners(){
@@ -43,6 +57,7 @@ class MainActivity : AppCompatActivity() {
                 applicationContext,
                 SavedImagesActivity::class.java
             ).also { intent ->
+                BREAK_LOOP = true
                 startActivity(intent)
             }
         }
@@ -57,12 +72,29 @@ class MainActivity : AppCompatActivity() {
                     EditImageActivity::class.java
                 ).also { editImageIntent ->
                     editImageIntent.putExtra(KEY_IMAGE_URi, imageUri)
+                    BREAK_LOOP = true
                     startActivity(editImageIntent)
                 }
             }
         }
     }
 
+    private fun changeBackgroundImage(){
+        GlobalScope.launch {
+            while (!BREAK_LOOP){
+                delay(8000L)
+                Log.d("Meeee", "changeBackgroundImage: ")
+                withContext(Dispatchers.Main){
+                    binding.imageBackground.animation = AnimationUtils.loadAnimation(applicationContext, R.anim.fade_in)
+                    binding.imageBackground.animate()
+                    binding.imageBackground.setImageResource(BackgroundImages.images[BACKGROUND_IMAGE_POSITION++])
+                    if(BACKGROUND_IMAGE_POSITION == BackgroundImages.images.size){
+                        BACKGROUND_IMAGE_POSITION = 0
+                    }
+                }
+            }
+        }
+    }
 
 
 }
---
+++ app/src/main/java/com/mallam/imagefilters/utilities/BackgroundImages.kt
@@ -0,0 +1,22 @@
+package com.mallam.imagefilters.utilities
+
+import com.mallam.imagefilters.R
+
+object BackgroundImages {
+    var images = listOf(
+        R.drawable.image_1,
+        R.drawable.image_2,
+        R.drawable.image_3,
+        R.drawable.image_4,
+        R.drawable.image_5,
+        R.drawable.image_6,
+        R.drawable.image_7,
+        R.drawable.image_8,
+        R.drawable.image_9,
+        R.drawable.image_10,
+        R.drawable.image_11,
+        R.drawable.image_12,
+        R.drawable.image_13,
+        R.drawable.image_14,
+        R.drawable.image_15)
+}
